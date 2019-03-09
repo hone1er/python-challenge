fa00761629b11d2abd1d53ac60e90620197e54ee
hone1er <josephmvillavicencio@gmail.com>
2019-03-09 12:13:12 -0800

    added month calculation

--- Pybank/main.py
+++ Pybank/main.py
@@ -4,8 +4,11 @@ import os
 file = os.path.join("..", "..", "Downloads","budget.csv")
 
 with open(file, 'r') as csvfile:
+    net = 0
+    month = 0
     csvreader = csv.reader(csvfile)
     header = next(csvfile)
-    net = 0
-    for x in csvreader:
-        net += int(x[1])
+    for row in csvreader:
+        month += 1
+        net += int(row[1])
+
