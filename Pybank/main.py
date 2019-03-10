801585413e732914ed6960bda4f697b42b9be2e2
hone1er <josephmvillavicencio@gmail.com>
2019-03-10 13:59:41 -0700

    added write to txt file

--- Pybank/main.py
+++ Pybank/main.py
@@ -3,13 +3,12 @@ import os
 from statistics import mean
 
 file = os.path.join('..', '..', 'Downloads','budget.csv')
-
+change = []
+months = []
+previous = 0
+net = 0
+month = 0
 with open(file, 'r') as csvfile:
-    change = []
-    months = []
-    previous = 0
-    net = 0
-    month = 0
     csvreader = csv.reader(csvfile)
     header = next(csvfile)
     for row in csvreader:
@@ -19,14 +18,18 @@ with open(file, 'r') as csvfile:
         change.append(diff)
         previous = int(row[1])
         months.append(row[0])
-    change.pop(0)
-    mx = max(change)
-    mn = min(change)
-    average_change = mean(change)
-    print(f'''Financial Analysis
+change.pop(0)
+mx = max(change)
+mn = min(change)
+average_change = mean(change)
+info = f'''Financial Analysis
 ----------------------------
 Total Months: {month}
 Total: ${net}
 Average  Change: ${round(average_change,2)}
 Greatest Increase in Profits: {months[change.index(max(change))+1]} (${mx})
-Greatest Decrease in Profits: {months[change.index(min(change))+1]} (${mn})''')
+Greatest Decrease in Profits: {months[change.index(min(change))+1]} (${mn})'''
+print(info)
+
+with open('completed_budget.txt', 'w') as completed:
+    completed.write(info)
