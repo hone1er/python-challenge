2b6de98310c738752cfb23acb8eb473bc24bf66a
hone1er <josephmvillavicencio@gmail.com>
2019-03-10 17:40:35 -0700

    switch from individual  variables to votes dict

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -4,35 +4,31 @@ import operator
 
 data = os.path.join('..', 'Resources', 'election_data.csv')
 vote_count = 0
-khan = 0
-correy = 0
-li = 0
-otooley = 0
+votes = {'Khan': 0, 'Correy': 0, 'Li': 0, "O'Tooley": 0}
 with open(data, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     header = next(csvfile)
     for row in csvreader:
         vote_count +=1
         if row[2] == 'Khan':
-            khan += 1
+            votes['Khan'] += 1
         elif row[2] == 'Correy':
-            correy +=1
+            votes['Correy'] +=1
         elif row[2] == 'Li':
-            li += 1
+            votes['Li'] += 1
         elif row[2] == "O'Tooley":
-            otooley +=1
-
-percents = {'Khan': (khan/vote_count)*100, 'Correy': (correy/vote_count)*100, 'Li': (li/vote_count)*100, "O'Tooley": (otooley/vote_count)*100}
-winner = max(percents.items(), key=operator.itemgetter(1))[0]
+            votes["O'Tooley"] +=1
+percents = {'Khan': (votes['Khan']/vote_count)*100, 'Correy': (votes['Correy']/vote_count)*100, 'Li': (votes['Li']/vote_count)*100, "O'Tooley": (votes["O'Tooley"]/vote_count)*100}
+winner = max(votes.items(), key=operator.itemgetter(1))[0]
 info = f"""
 Election Results
 -------------------------
 Total Votes: {vote_count}
 -------------------------
-Khan: {round(percents['Khan'],3)}% ({khan})
-Correy: {round(percents['Correy'],3)}% ({correy})
-Li: {round(percents['Li'],3)}% ({li})
-O'Tooley: {round(percents["O'Tooley"],3)}% ({otooley})
+Khan: {round(percents['Khan'],3)}% ({votes['Khan']})
+Correy: {round(percents['Correy'],3)}% ({votes['Correy']})
+Li: {round(percents['Li'],3)}% ({votes['Li']})
+O'Tooley: {round(percents["O'Tooley"],3)}% ({votes["O'Tooley"]})
 -------------------------
 Winner: {winner}
 -------------------------
