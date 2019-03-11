a7dbf372daf0c6627aefaed260110c48e5e179fb
hone1er <josephmvillavicencio@gmail.com>
2019-03-10 22:58:52 -0700

    removed for loop

--- Pypoll/main.py
+++ Pypoll/main.py
@@ -1,32 +1,26 @@
 import csv
 import os
 import operator
+import timeit
 
 data = os.path.join('..', 'Resources', 'election_data.csv')
 vote_count = 0
-# list of candidates(using this method allows you to add a name to the list of candidates)
+# list of candidates. If a name is added to the csv, add it to this list.
 candidates = ['Khan', 'Correy', 'Li', "O'Tooley"] 
 # dictionary to store candidate: vote_count
 votes = {}
-# dictionary to store candidate: vote percentage
-percent = {}
-# fill votes dict using the list of candidates and set their votes to 0
-for name in candidates:
-    votes[name] = 0
-
 with open(data, 'r') as csvfile:
     csvreader = csv.reader(csvfile)
     header = next(csvfile)
     for row in csvreader:
         vote_count +=1
-        # check if candidate in votes dict, if the are, add 1 to their vote count
-        if row[2] in votes:
+        # check if candidate in votes dict, if the are, add 1 to their vote count, if not, add them to the dict with 1 vote
+        if row[2] not in votes:
+            votes[row[2]] = 1
+        else:
             votes[row[2]] += 1
-# populate the percent dict using a for loop through the candidate list and create table of candidate stats
-table = ""
-for name in candidates:
-    percent[name] = (votes[name]/vote_count)*100
-    table += (f"\n{name}: {round(percent[name],3)}% ({format(votes[name], ',d')})")
+# create table of candidate stats
+table = "\n".join([f"{name}: {round((votes[name]/vote_count)*100,3)}% ({format(votes[name], ',d')})" for name in candidates])
 # get the max value in the votes dict and return the key
 winner = max(votes.items(), key=operator.itemgetter(1))[0]
 info = f"""
@@ -35,7 +29,6 @@ Election Results
 Total Votes: {format(vote_count, ',d')}
 -------------------------
 {table}
-
 -------------------------
 Winner: {winner}
 -------------------------
